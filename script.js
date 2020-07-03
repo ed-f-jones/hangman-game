a8aad8a639b80f742511040428ffc50834a54b29
unknown <ed.f.jones@gmail.com>
2020-07-03 14:11:07 -0400

    show word and notifications functions complete

---
+++ script.js
@@ -0,0 +1,84 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification  = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = [
+    'javascript',
+    'programming',
+    'react',
+    'websites',
+    'interface',
+    'portraits',
+    'photography',
+    'picture',
+    'editing',
+    'photoshop'
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// show hiiden word
+function displayWord() {
+wordEl.innerHTML = `
+${selectedWord.split('').map(letter => `
+    <span class="letter">
+    ${correctLetters.includes(letter) ? letter : ''}
+    </span>
+`).join('')}
+`;
+const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+    if ( innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations! you won!  😃';
+        popup.style.display = 'flex';
+    }
+}
+
+// update wrong letters
+function updateWrongLettersEl() {
+    console.log('update wrong');
+};
+
+// show notification
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(()=> {
+        notification.classList.remove('show');
+    }, 2000)
+};
+
+// keydown letter press
+window.addEventListener('keydown', e => {
+    // console.log(e.keyCode);
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+        if(selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+    
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+    } 
+    }
+});
+
+displayWord();
